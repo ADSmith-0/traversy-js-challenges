78894ec385b6c1ff1104714e587640cb613f6c39
adam smith <adam00199382818@gmail.com>
2023-10-12 09:32:43 +0100

    completed 02-02

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,22 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  // Declarative
+  // return arr1.filter((val) => arr2.includes(val));
+
+  // Imperative
+  const returnArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      const arr1Num = arr1[i];
+      const arr2Num = arr2[j];
+
+      if (arr1Num === arr2Num) {
+        returnArr.push(arr1Num);
+      }
+    }
+  }
+
+  return returnArr;
+}
 
 module.exports = arrayIntersection;
