e476db3e16fe2badb1f5e9e1e9388fe0ae417d70
adam smith <adam00199382818@gmail.com>
2023-10-12 09:37:19 +0100

    completed 03-10

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  // Decided not doing it with HOA as for loop is way more efficient
+  
+  let prevCharVal = arr[0].charCodeAt(0);
+
+  for (let i = 1; i < arr.length; i++) {
+    const char = arr[i];
+    const charVal = char.charCodeAt(0);
+
+    if (charVal - prevCharVal > 1) {
+      return String.fromCharCode(prevCharVal + 1);
+    }
+
+    prevCharVal++;
+  }
+}
 
 module.exports = findMissingLetter;
