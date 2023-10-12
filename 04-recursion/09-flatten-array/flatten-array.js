f0ab53984c31ed6a7081afac2562fe8bbab69b34
adam smith <adam00199382818@gmail.com>
2023-10-12 18:21:55 +0100

    completed 04-09

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,17 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const newArr = flattenArray(arr.slice(1));
+
+  if (Array.isArray(arr[0])) {
+    newArr.unshift(...flattenArray(arr[0]));
+  } else {
+    newArr.unshift(arr[0]);
+  }
+
+  return newArr;
+}
 
 module.exports = flattenArray;
