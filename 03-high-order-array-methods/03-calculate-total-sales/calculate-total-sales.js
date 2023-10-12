5d5c616e0c95a22d58fc6c5c338b597d7c09a0d9
adam smith <adam00199382818@gmail.com>
2023-10-12 09:35:20 +0100

    completed 03-03

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,5 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  return products.reduce((sum, num) => sum + num.price * num.quantity, 0) * ((taxRate / 100) + 1);
+}
 
 module.exports = calculateTotalSalesWithTax;
