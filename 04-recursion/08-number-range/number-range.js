037d7ed7de0be48094583231d302781aaee238ae
adam smith <adam00199382818@gmail.com>
2023-10-12 11:07:11 +0100

    completed 04-08

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,19 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  }
+
+  return [startNum, ...numberRange(startNum + 1, endNum)];
+
+  // if (startNum === endNum) {
+  //   return [startNum];
+  // }
+  //
+  // const numbers = numberRange(startNum, endNum - 1);
+  //
+  // numbers.push(endNum);
+  //
+  // return numbers;
+}
 
 module.exports = numberRange;
