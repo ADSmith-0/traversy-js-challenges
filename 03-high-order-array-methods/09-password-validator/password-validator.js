64f9497ea4afd6a6eeedf337cc1039aff77f30d0
adam smith <adam00199382818@gmail.com>
2023-10-12 09:37:08 +0100

    completed 03-09

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,21 @@
-function validatePassword() {}
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+
+  const hasCapital = !!password.match(/[A-Z]/g)?.length ?? false;
+  const hasLowerCase = !!password.match(/[a-z]/g)?.length ?? false;
+  const hasDigit = !!password.match(/[0-9]/g)?.length ?? false;
+
+  return hasCapital && hasLowerCase && hasDigit;
+
+  // const hasCaptial = false;
+  // const hasLowerCase = false;
+  // const hasDigit = false;
+  //
+  // for (let i = 0; i < password.length; i++) {
+  //    etc... more efficient
+  // }
+}
 
 module.exports = validatePassword;
