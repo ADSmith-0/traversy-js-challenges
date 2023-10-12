cbcd0a8fcea3250f481cdd0f197f5cfd97a5b26a
adam smith <adam00199382818@gmail.com>
2023-10-12 09:33:58 +0100

    completed 02-08

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,33 @@
-function diceGameSimulation() {}
+function getResult(num) {
+  if (num === 7 || num === 11) {
+    return "win";
+  }
+
+  if (num === 2 || num === 3 || num === 12) {
+    return "lose";
+  }
+
+  return "roll again";
+}
+
+function diceGameSimulation(numOfSimulations) {
+  const results = [];
+
+  for (let i = 0; i < numOfSimulations; i++) {
+    const dice1 = Math.ceil(Math.random() * 6);
+    const dice2 = Math.ceil(Math.random() * 6);
+    const sum = dice1 + dice2;
+    const result = getResult(dice1 + dice2);
+
+    results.push({
+      dice1,
+      dice2,
+      sum,
+      result,
+    });
+  }
+
+  return results;
+}
 
 module.exports = diceGameSimulation;
