edb0cac56fd9dee960f8b3b495e99ba56e5945c7
adam smith <adam00199382818@gmail.com>
2023-10-12 09:35:35 +0100

    completed 03-04

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,17 @@
-function highestScoringWord() {}
+const valFromChar = (char) => {
+  return char.toLowerCase().charCodeAt(0) - 96;
+}
+
+function highestScoringWord(arr) {
+  const words = arr.split(" ");
+
+  const wordsScores = words.map((word) => word.split("").reduce((sum, char) => sum + valFromChar(char), 0));
+
+  const maxScore = Math.max(...wordsScores);
+
+  const index = wordsScores.indexOf(maxScore);
+
+  return words[index];
+}
 
 module.exports = highestScoringWord;
