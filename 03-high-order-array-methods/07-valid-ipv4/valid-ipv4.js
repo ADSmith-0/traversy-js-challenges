0900d02a40b9b802d342ffba0483474807d4a08b
adam smith <adam00199382818@gmail.com>
2023-10-12 09:36:43 +0100

    completed 03-07

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ipv4Str) => {
+  const octets = ipv4Str.split(".");
+
+  if (octets.length !== 4) {
+    return false;
+  }
+
+  return octets.every((octet) => octet >= 0 && octet <= 255 && octet === parseInt(octet).toString());
+
+};
 
 module.exports = isValidIPv4;
