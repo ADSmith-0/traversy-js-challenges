a13a8b970bf6047f986e7353b8aaa60b9c071a19
adam smith <adam00199382818@gmail.com>
2023-10-12 09:35:48 +0100

    completed 03-05

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,21 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const str1ArrSorted = str1.split("").sort();
+  const str2ArrSorted = str2.split("").sort();
+
+  for (let i = 0; i < str1ArrSorted.length; i++) {
+    const str1Char = str1ArrSorted[i];
+    const str2Char = str2ArrSorted[i];
+
+    if (str1Char !== str2Char) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = validAnagrams;
