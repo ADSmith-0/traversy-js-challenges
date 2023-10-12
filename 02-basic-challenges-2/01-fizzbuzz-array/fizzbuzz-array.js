16ceeceedc21bc2ec7d29a65a0f87e98f1292159
adam smith <adam00199382818@gmail.com>
2023-10-12 09:32:32 +0100

    completed 02-01

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,28 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const fizzBuzzArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (!(i % 15)) {
+      fizzBuzzArr[i] = "FizzBuzz";
+      continue;
+    }
+
+    if (!(i % 3)) {
+      fizzBuzzArr[i] = "Fizz";
+      continue;
+    }
+
+    if (!(i % 5)) {
+      fizzBuzzArr[i] = "Buzz";
+      continue;
+    }
+
+    fizzBuzzArr[i] = i;
+  }
+
+  fizzBuzzArr.shift();
+
+  return fizzBuzzArr;
+}
 
 module.exports = fizzBuzzArray;
