4188ddfa3f5ab2b1394577a4dbd557b97b8cb898
adam smith <adam00199382818@gmail.com>
2023-10-12 09:30:53 +0100

    completed 01-08

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,19 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  // let isPalindrome = true;
+  //
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i] !== str[str.length - (1 + i)]) {
+  //     isPalindrome = false;
+  //     break;
+  //   }
+  // }
+  // return isPalindrome;
+
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  const reversedStr = cleanedStr.split("").reverse().join("");
+
+  return reversedStr === cleanedStr;
+}
 
 module.exports = isPalindrome;
