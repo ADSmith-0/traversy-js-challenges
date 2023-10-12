164bfc445c8712d7f3e8a2017950d185c2393800
adam smith <adam00199382818@gmail.com>
2023-10-12 09:29:45 +0100

    completed 01-05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+  // Simple way
+  // return Math.max(...numbers);
+
+  // More complicated way
+  let maxNumber = numbers[0];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] > numbers[i+1]) {
+      maxNumber = numbers[i];
+    } else if (numbers[i + 1] > numbers[i]) {
+      maxNumber = numbers[i + 1];
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
