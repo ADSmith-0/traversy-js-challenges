a4ebbdd3cb77bf77bb2d8d3ed8f53a8e1a3c9ef3
adam smith <adam00199382818@gmail.com>
2023-10-12 09:31:28 +0100

    completed 01-09

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,17 @@
-function countVowels() {}
+function countVowels(str) {
+  // Declarative
+  // return str.toLowerCase().split("").filter((char) => char === "a" || char === "e" || char === "o" || char === "i" || char === "u").length;
+
+  // Imperative
+  let count = 0;
+  const lowerStr = str.toLowerCase();
+
+  for (let i = 0; i < lowerStr.length; i++) {
+    if (lowerStr[i] === "a" || lowerStr[i] === "e" || lowerStr[i] === "i" || lowerStr[i] === "o" || lowerStr[i] === "u") {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
