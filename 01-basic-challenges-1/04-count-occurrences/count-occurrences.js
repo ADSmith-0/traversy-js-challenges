1b25005692d699a281c5b21e3951930ac31fc6a8
adam smith <adam00199382818@gmail.com>
2023-10-12 09:29:23 +0100

    completed 01-04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,5 @@
-function countOccurrences() {}
+function countOccurrences(inputString, char) {
+  return inputString.split("").filter((ch) => ch === char).length;
+}
 
 module.exports = countOccurrences;
