11c758dc5625e56fe69028f665e646abdd66f3f1
adam smith <adam00199382818@gmail.com>
2023-10-12 09:36:57 +0100

    completed 03-08

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,21 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const mileages = cars.map((car) => car.mileage);
+
+  const totalMileage = mileages.reduce((sum, num) => sum + num, 0) ;
+  const averageMileage = totalMileage / mileages.length;
+
+  const highestMileage = Math.max(...mileages);
+  const highestMileageCar = cars.find((car) => car.mileage === highestMileage);
+
+  const lowestMileage = Math.min(...mileages);
+  const lowestMileageCar = cars.find((car) => car.mileage === lowestMileage);
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage
+  }
+}
 
 module.exports = analyzeCarMileage;
