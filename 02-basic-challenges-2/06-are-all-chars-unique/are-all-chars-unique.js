700e61a63ba673b78eda17c7bc5e3ad7b0a4dd90
adam smith <adam00199382818@gmail.com>
2023-10-12 09:33:31 +0100

    completed 02-06

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,33 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  // O(n^2) implementation
+  // const chars = [];
+  //
+  // for (let i = 0; i < str.length; i++) {
+  //   const char = str[i];
+  //
+  //   if (chars.includes(char)) {
+  //     return false;
+  //   }
+  //
+  //   chars.push(char);
+  // }
+  //
+  // return true;
+
+  // O(n) implementation
+  const chars = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (chars.has(char)) {
+      return false;
+    }
+
+    chars.set(char, true);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
