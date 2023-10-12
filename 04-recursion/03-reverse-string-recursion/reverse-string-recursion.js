386439023d3f168563d3998d7db6e2fcd0c7658b
adam smith <adam00199382818@gmail.com>
2023-10-12 10:56:21 +0100

    completed 04-04

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+
+  // return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
+  return reverseString(str.substr(1)) + str.charAt(0);
+}
 
 module.exports = reverseString;
