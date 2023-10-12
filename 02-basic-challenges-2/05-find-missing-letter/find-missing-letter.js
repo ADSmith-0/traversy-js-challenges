f25b7f49200b02800fca8998269aad7337593787
adam smith <adam00199382818@gmail.com>
2023-10-12 09:33:20 +0100

    completed 02-05

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(charArr) {
+
+  for (let i = 0; i < charArr.length - 1; i++) {
+    const prevValue = charArr[i].charCodeAt(0);
+    const currValue = charArr[i + 1].charCodeAt(0);
+
+    if (currValue - prevValue > 1) {
+      return String.fromCharCode((currValue + prevValue) / 2);
+    }
+  }
+
+  return "";
+}
 
 module.exports = findMissingLetter;
