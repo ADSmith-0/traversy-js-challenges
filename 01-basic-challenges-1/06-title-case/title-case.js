7fd0f595c569cd7476177c7dbe8e30039ce21ce6
adam smith <adam00199382818@gmail.com>
2023-10-12 09:29:58 +0100

    completed 01-06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,16 @@
-function titleCase() {}
+function titleCase(str) {
+  // Declarative
+  return str.split(" ").map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+
+  // Imperative
+  // const words = str.split(" ");
+  // const titleCaseWords = [];
+  //
+  // for (let i = 0; i < words.length; i++) {
+  //   titleCaseWords[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+  // }
+  //
+  // return titleCaseWords.join(" ");
+}
 
 module.exports = titleCase;
