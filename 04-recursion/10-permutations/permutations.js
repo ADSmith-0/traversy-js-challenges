898b942388ad9886fd905f063094bf81b0cab226
adam smith <adam00199382818@gmail.com>
2023-10-12 18:22:13 +0100

    completed 04-10

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,21 @@
-function permutations() {}
+function permutations(str) {
+  if (str.length <= 1) {
+    return [str];
+  }
+
+  const allPerms = [];
+
+  for (const char of str) {
+    const newStr = str.replace(char, "");
+
+    const perms = permutations(newStr);
+
+    for (const perm of perms) {
+      allPerms.push(char + perm);
+    }
+  }
+  
+  return allPerms;
+}
 
 module.exports = permutations;
