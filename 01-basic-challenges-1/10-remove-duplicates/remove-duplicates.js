34406a3b3a0ff1abbde9253177ff96666d5b2468
adam smith <adam00199382818@gmail.com>
2023-10-12 09:31:40 +0100

    completed 01-10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,28 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  // With Sets
+  // return Array.from(new Set(arr));
+
+  // With for loop
+  const duplicates = [];
+  const set = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (duplicates.includes(arr[i])) {
+      continue;
+    }
+
+    set.push(arr[i]);
+    duplicates.push(arr[i]);
+  }
+  
+  // Better implementation from solution
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (!set.includes(arr[i])) {
+  //     set.push(arr[i]);
+  //   }
+  // }
+
+  return set;
+}
 
 module.exports = removeDuplicates;
