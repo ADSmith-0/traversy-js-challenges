3e9576d5824b917f3c58864e496003f65af2caeb
adam smith <adam00199382818@gmail.com>
2023-10-12 09:33:09 +0100

    completed 02-04

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,15 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (!arr) return undefined;
+
+  const n = arr.length;
+
+  if (n === 0) return 1;
+
+  const expectedLength = (n + 1) * (n + 2) / 2;
+
+  const actualLength = arr.reduce((sum, num) => sum += num, 0);
+
+  return expectedLength - actualLength;
+}
 
 module.exports = findMissingNumber;
