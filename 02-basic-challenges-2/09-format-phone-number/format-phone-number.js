60b43472109fcee56ed2c9d4dac0daa7baa796d9
adam smith <adam00199382818@gmail.com>
2023-10-12 09:34:08 +0100

    completed 02-09

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  const firstThird = arr.slice(0, 3);
+  const secondThird = arr.slice(3, 6);
+  const thirdThird = arr.slice(6);
+
+  return `(${firstThird.join("")}) ${secondThird.join("")}-${thirdThird.join("")}`;
+}
 
 module.exports = formatPhoneNumber;
