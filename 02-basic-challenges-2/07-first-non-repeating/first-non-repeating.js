3ff8b01da659de92573f485aea1f93c0d9d5dc64
adam smith <adam00199382818@gmail.com>
2023-10-12 09:33:48 +0100

    completed 02-07

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,25 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  let lastChar = str[0];
+  let count = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    const char = str[i];
+
+    if (lastChar !== char) {
+      if (count === 1) {
+        return lastChar;
+      }
+
+      count = 1;
+      lastChar = char;
+      continue;
+    }
+
+    count++;
+    
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
