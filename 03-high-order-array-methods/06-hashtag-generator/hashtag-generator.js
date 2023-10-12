a4f9730d8064e72a723a0619272648e3abd2c973
adam smith <adam00199382818@gmail.com>
2023-10-12 09:35:57 +0100

    completed 03-06

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,9 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (!str || str.length > 140) {
+    return false;
+  }
+
+  return "#" + str.split(" ").filter(Boolean).map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()).join("");
+}
 
 module.exports = generateHashtag;
