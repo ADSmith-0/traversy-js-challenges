4d9017ce4a4d07a416f46f953d0539298e6ebacc
adam smith <adam00199382818@gmail.com>
2023-10-12 09:34:45 +0100

    completed 03-02

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,6 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  // O(n)
+  return arr.reduce((sum, num) => num % 2 === 0 ? sum += num**2 : sum, 0);
+}
 
 module.exports = sumOfEvenSquares;
