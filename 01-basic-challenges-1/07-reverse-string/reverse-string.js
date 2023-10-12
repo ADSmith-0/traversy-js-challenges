873d6ba80c028dbe3715cf90404137426e881822
adam smith <adam00199382818@gmail.com>
2023-10-12 09:30:16 +0100

    completed 01-07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(inputStr) {
+  const reversedStr = [];
+
+  for (let i = inputStr.length - 1; i >= 0; i--) {
+    reversedStr.push(inputStr[i]);
+  }
+
+  return reversedStr.join("");
+}
 
 module.exports = reverseString;
