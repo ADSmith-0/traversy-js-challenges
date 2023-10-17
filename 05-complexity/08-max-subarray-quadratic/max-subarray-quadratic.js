998dab4079903d72a13d9b36b1a92f4ffc8818b7
adam smith <adam00199382818@gmail.com>
2023-10-18 00:05:57 +0100

    completed 05-08

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,20 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  // I think that this might still be O(n)?
+  // Essentially arr[i] + arr[i + 1] + arr[i + 2];
+
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < arr.length - k; i++) {
+    let currSum = 0;
+
+    for (let j = i; j < i + k; j++) {
+      currSum += arr[j];
+    }
+
+    maxSum = Math.max(currSum, maxSum);
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
