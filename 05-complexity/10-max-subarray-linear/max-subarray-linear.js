f2ea161844e6fa855e05dbf786db7b660ced899a
adam smith <adam00199382818@gmail.com>
2023-10-18 19:00:17 +0100

    completed 05-10

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,18 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+
+  let currSum = maxSum;
+
+  for (let i = k; i < arr.length; i++) {
+    currSum = currSum - arr[i - k] + arr[i];
+    maxSum = Math.max(currSum, maxSum);
+  }
+
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
