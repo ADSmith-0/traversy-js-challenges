71e2b4b6ff5c43a71f28d749f20d25391aa2cb77
adam smith <adam00199382818@gmail.com>
2023-10-20 00:39:58 +0100

    completed 06-04

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,17 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  const frequencyMap = new Map();
+
+  if (str.length === 0) {
+    return frequencyMap;
+  }
+
+  for (const word of str.split(" ")) {
+    const wordKey = word.toLowerCase().replace(/[!|\.|\,]/g, "");
+    const wordFrequency = frequencyMap.get(wordKey) ?? 0;
+    frequencyMap.set(wordKey, wordFrequency + 1);
+  }
+
+  return frequencyMap;
+}
 
 module.exports = wordFrequencyCounter;
