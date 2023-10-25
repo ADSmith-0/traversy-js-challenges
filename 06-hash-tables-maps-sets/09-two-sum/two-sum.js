96a9f63c1cf2a667600bddfc2583052b31b8dc37
adam smith <adam00199382818@gmail.com>
2023-10-25 12:29:33 +0100

    completed 06-09

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,34 @@
-function twoSum() {}
+function twoSum(nums, target) {
+  // for (let i = 0; i < nums.length; i++) {
+  //   const num1 = nums[i];
+  //
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     const num2 = nums[j];
+  //     
+  //     const sum = num1 + num2;
+  //
+  //     if (sum === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+
+  const complements = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+
+    const num = nums[i];
+
+    const complement = target - num;
+
+    if (complements.has(complement)) {
+      return [nums.indexOf(complement), i];
+    }
+
+    complements.add(num);
+  }
+
+  return [];
+}
 
 module.exports = twoSum;
