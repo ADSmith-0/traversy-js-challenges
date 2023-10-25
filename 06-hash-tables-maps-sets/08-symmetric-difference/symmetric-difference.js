fc699b5ed166891beaa6fb093a121b0abab3abf4
adam smith <adam00199382818@gmail.com>
2023-10-25 10:16:17 +0100

    completed 06-08

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,20 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const resultArr = [];
+
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  for (const val of set1.values()) {
+    if (set2.has(val)) {
+      set1.delete(val);
+      set2.delete(val);
+    }
+  }
+
+  const finalArr1 = Array.from(set1);
+  const finalArr2 = Array.from(set2);
+
+  return finalArr1.concat(finalArr2);
+}
 
 module.exports = symmetricDifference;
