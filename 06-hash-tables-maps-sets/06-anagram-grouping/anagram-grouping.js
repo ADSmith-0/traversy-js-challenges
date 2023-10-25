93e852b479de2440474541d1d975b1e2d6ea83d3
adam smith <adam00199382818@gmail.com>
2023-10-25 10:15:22 +0100

    completed 06-06

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,3 +1,21 @@
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  const words = new Map();
+  
+  for (let i = 0; i < arr.length; i++) {
+    const word = arr[i];
+    const key = word.split("").sort().join("");
+
+    const currentVal = words.get(key);
+
+    if (currentVal) {
+      currentVal.push(word);
+      words.set(key, currentVal);
+    } else {
+      words.set(key, [word]);
+    }
+  }
+
+  return Array.from(words.values());
+}
 
 module.exports = anagramGrouping;
