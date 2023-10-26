e7de9f4cbb25ff9ed029d723a8160fd766c97012
adam smith <adam00199382818@gmail.com>
2023-10-26 16:34:13 +0100

    completed 06-11

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,43 @@
-class HashTable {}
+class HashTable {
+  constructor(limit = 14) {
+    this.storage = [];
+    this.limit = limit;
+  }
+
+  #hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+
+    return hash % this.limit;
+  }
+
+  set(key, value) {
+    const index = this.#hash(key);
+    this.storage[index] = value;
+  }
+
+  get(key) {
+    const index = this.#hash(key);
+    return this.storage[index];
+  }
+
+  remove(key) {
+    const index = this.#hash(key);
+    delete this.storage[index];
+  }
+
+  getValues() {
+    return this.storage.filter(Boolean);
+  }
+}
+
+const ht = new HashTable(8);
+ht.set('john', '555-234-3544');
+ht.set('james', '555-384-5523');
+ht.set('jack', '555-384-9933');
+ht.set('jill', '555-390-0034');
+ht.set('james', '555-384-0011');
 
 module.exports = HashTable;
