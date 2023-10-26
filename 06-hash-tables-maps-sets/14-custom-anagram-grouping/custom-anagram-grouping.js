a56d8a21344fc0effa68c139a9e199bef443d693
adam smith <adam00199382818@gmail.com>
2023-10-26 19:13:27 +0100

    completed 06-14

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,20 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+  const hashTable = new HashTable(words.length);
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    const key = word.split("").sort().join("");
+
+    const value = hashTable.get(key) ?? [];
+    value.push(word);
+
+    hashTable.set(key, value);
+  }
+
+  return hashTable.getValues();
+}
 
 module.exports = anagramGrouping;
