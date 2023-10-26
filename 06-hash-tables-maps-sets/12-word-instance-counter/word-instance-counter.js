3194a5834189692a0f370a97ce4e22d3f08276ab
adam smith <adam00199382818@gmail.com>
2023-10-26 17:05:35 +0100

    completed 06-12

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,22 @@
 const HashTable = require('./HashTable');
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, targetWord) {
+  // Decided not to use a HashTable here as we only need to count the instance of 1 word,
+  // so the rest of the HashTable instance is unnecessary
+  let count = 0;
+
+  const sanitisedStr = str.replace(/[!|.|,]/g, "").toLowerCase();
+  const words = sanitisedStr.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (word === targetWord) {
+      count++;
+    }
+  }
+
+  return count;
+
+}
 
 module.exports = wordInstanceCounter;
