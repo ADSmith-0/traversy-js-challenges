f1d7527d4471353110e1f2858d5c3b60f55fce15
adam smith <adam00199382818@gmail.com>
2023-10-26 15:17:59 +0100

    completed 06-10

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,22 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+  const numSet = new Set(nums);
+
+  let longestSequence = 0;
+  let currentSequence = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+
+    while (numSet.has(num + 1)) {
+      currentSequence++;
+      num++;
+    }
+
+    longestSequence = Math.max(longestSequence, currentSequence);
+    currentSequence = 1;
+  }
+
+  return longestSequence;
+}
 
 module.exports = longestConsecutiveSequence;
