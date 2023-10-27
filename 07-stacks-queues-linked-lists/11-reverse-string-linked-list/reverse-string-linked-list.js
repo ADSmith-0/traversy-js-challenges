85b30c0cdcf39faaadbd2c22dd569a5d50fac90d
adam smith <adam00199382818@gmail.com>
2023-10-27 15:35:50 +0100

    completed 07-11

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,21 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  let outputStr = "";
+  const linkedList = new LinkedList();
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str[i];
+    linkedList.add(char);
+  }
+
+  let current = linkedList.head;
+  while (current !== null) {
+    outputStr += current.data;
+    current = current.next;
+  }
+
+  return outputStr;
+}
 
 module.exports = reverseStringLinkedList;
