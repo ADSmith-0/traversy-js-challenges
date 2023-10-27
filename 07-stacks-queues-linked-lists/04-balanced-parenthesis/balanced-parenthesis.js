54252836ce38bcbe6dbad3738905f456cb2c7fa0
adam smith <adam00199382818@gmail.com>
2023-10-27 10:27:43 +0100

    completed 07-04

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,25 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+  const stack = new Stack();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char === "(") {
+      stack.push(char);
+    }
+
+    if (char === ")") {
+      const bracket = stack.pop();
+
+      if (bracket === null) {
+        return false;
+      }
+    }
+  }
+
+  return stack.isEmpty();
+}
 
 module.exports = balancedParenthesis;
