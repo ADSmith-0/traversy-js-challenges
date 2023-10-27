295036b8769b81d316a517e293c51f1923af0ad6
adam smith <adam00199382818@gmail.com>
2023-10-27 15:18:25 +0100

    completed 07-08

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,25 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+  const queue = new Queue();
+  const stack = new Stack();
+
+  const sanitisedStr = str.replace(/[\s|,]/g, "").toLowerCase();
+  
+  for (let i = 0; i < sanitisedStr.length; i++) {
+    const char = sanitisedStr[i];
+    queue.enqueue(char);
+    stack.push(char);
+  }
+
+  while (!queue.isEmpty()) {
+    if (queue.dequeue() !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = isPalindromeQueueStack;
