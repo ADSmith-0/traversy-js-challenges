279791e473c2d5b0097d411b8b326f4b7807b4cd
adam smith <adam00199382818@gmail.com>
2023-10-27 15:48:11 +0100

    completed 07-13

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,13 @@
-function findMiddle() {}
+function findMiddle(list) {
+  let slowPtr = list.head;
+  let fastPtr = list.head;
+
+  while (fastPtr !== null && fastPtr.next !== null) {
+    slowPtr = slowPtr.next;
+    fastPtr = fastPtr.next.next;
+  }
+
+  return slowPtr;
+}
 
 module.exports = findMiddle;
