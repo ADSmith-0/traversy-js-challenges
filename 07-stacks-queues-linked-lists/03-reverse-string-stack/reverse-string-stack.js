6d51e74d642a1c14fb6ce549062649217c2436f7
adam smith <adam00199382818@gmail.com>
2023-10-27 10:27:29 +0100

    completed 07-03

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,19 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack();
+  let outputStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    stack.push(char);
+  }
+
+  while (!stack.isEmpty()) {
+    outputStr += stack.pop();
+  }
+
+  return outputStr;
+}
 
 module.exports = reverseStringStack;
