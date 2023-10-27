f5c0844328b5e47a0a077ceac57eebc50bf8fe3f
adam smith <adam00199382818@gmail.com>
2023-10-27 10:28:21 +0100

    completed 07-07

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,19 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  const queue = new Queue();
+  let outputStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str[i];
+    queue.enqueue(char);
+  }
+
+  while (!queue.isEmpty()) {
+    outputStr += queue.dequeue();
+  }
+
+  return outputStr;
+};
 
 module.exports = reverseStringWithQueue;
