98edde90a60af0f2e7fcd31854e9d2973b53a570
adam smith <adam00199382818@gmail.com>
2023-10-30 18:32:01 +0000

    completed 07-16

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,20 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
-function findPairSum() {}
+function findPairSum(numbers, targetSum) {
+  const list = new DoublyLinkedList();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    const target = targetSum - num;
+
+    if (list.contains(target)) {
+      return [target, num];
+    }
+
+    list.append(num);
+  }
+
+  return null;
+}
 
 module.exports = findPairSum;
