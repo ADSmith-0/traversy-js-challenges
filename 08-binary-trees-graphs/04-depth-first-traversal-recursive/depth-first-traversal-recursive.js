910e1568008c3a88c6bb1acaf64ccddfc1afc408
adam smith <adam00199382818@gmail.com>
2023-10-31 11:20:26 +0000

    completed 08-04

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,6 +1,30 @@
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function recDepthFirstTraversal() {}
+function recDepthFirstTraversal(root) {
+  if (!root) {
+    return;
+  }
+
+  const result = [];
+
+  result.push(root.data);
+
+  if (root.left) {
+    result.push(...recDepthFirstTraversal(root.left));
+  }
+
+  if (root.right) {
+    result.push(...recDepthFirstTraversal(root.right));
+  }
+
+  return result;
+}
 
 module.exports = {
   Node,
