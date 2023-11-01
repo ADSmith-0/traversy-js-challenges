ec2fa1ddefe3875c6dc103bb2f4a650c56df185c
adam smith <adam00199382818@gmail.com>
2023-11-01 10:56:57 +0000

    completed 08-06

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth.js
@@ -1,6 +1,29 @@
-class Node {}
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function maxDepth() {}
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+
+  let leftTree = 1;
+  let rightTree = 1;
+
+  if (root.left) {
+    leftTree = 1 + maxDepth(root.left);
+  }
+
+  if (root.right) {
+    rightTree = 1 + maxDepth(root.right);
+  }
+
+  return Math.max(leftTree, rightTree);
+}
 
 module.exports = {
   maxDepth,
