a6f8fcd52f6479ffc420faa48cfa8fb2e60a4b99
adam smith <adam00199382818@gmail.com>
2023-11-02 17:28:05 +0000

    completed 08-14

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,33 @@
 const Stack = require('./stack');
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, startingVertex) {
+  if (Object.keys(graph.adjacencyList).length === 0) {
+    return [];
+  }
+
+  const stack = new Stack();
+  const visitedVertices = new Set();
+  const path = [];
+
+  stack.push(startingVertex);
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+
+    if (!visitedVertices.has(currentVertex)) {
+      path.push(currentVertex);
+    }
+
+    for (const vertex of graph.adjacencyList[currentVertex]) {
+      if (!visitedVertices.has(vertex)) {
+        stack.push(vertex);
+      }
+    }
+
+    visitedVertices.add(currentVertex);
+  }
+
+  return path;
+}
 
 module.exports = depthFirstTraversal;
