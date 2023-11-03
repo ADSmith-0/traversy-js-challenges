32390035a6452eed1a886b19365500b429b00a50
adam smith <adam00199382818@gmail.com>
2023-11-03 09:44:35 +0000

    completed 09-03

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,22 @@
-function bubbleSort() {}
+function bubbleSort(numbers) {
+  let hasNextPass = true;
+
+  while (hasNextPass) {
+    hasNextPass = false;
+    
+    for (let i = 0; i < numbers.length - 1; i++) {
+      const number = numbers[i];
+      const nextNumber = numbers[i + 1];
+
+      if (nextNumber < number) {
+        numbers[i] = nextNumber;
+        numbers[i + 1] = number;
+        hasNextPass = true;
+      }
+    }
+  }
+
+  return numbers;
+}
 
 module.exports = bubbleSort;
