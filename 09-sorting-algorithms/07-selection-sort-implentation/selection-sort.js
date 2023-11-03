b4a2c61180c8b711cbee1b176ce70b4c75eb4a59
adam smith <adam00199382818@gmail.com>
2023-11-03 12:51:36 +0000

    completed 09-07

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,15 @@
-function selectionSort() {}
+function selectionSort(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    const minNumber = Math.min(...numbers.slice(i));
+
+    const indexOfMin = numbers.lastIndexOf(minNumber);
+
+    numbers[i] = minNumber;
+    numbers[indexOfMin] = number;
+  }
+
+  return numbers;
+}
 
 module.exports = selectionSort;
