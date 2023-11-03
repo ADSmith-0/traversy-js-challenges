016eb07dfccb277666fbad6f4e39e0d6283e9c23
adam smith <adam00199382818@gmail.com>
2023-11-03 14:54:52 +0000

    completed 09-11

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,27 @@
-function quickSort() {
+function quickSort(numbers) {
+  if (numbers.length <= 1) {
+    return numbers;
+  }
 
+  const pivot = numbers[0];
+
+  const leftArr = [];
+  const rightArr = [];
+  
+  for (let i = 1; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    if (number < pivot) {
+      leftArr.push(number);
+    } else {
+      rightArr.push(number);
+    }
+  }
+
+  const sortedLeftArr = quickSort(leftArr);
+  const sortedRightArr = quickSort(rightArr);
+
+  return [...sortedLeftArr, pivot, ...sortedRightArr];
 }
 
 module.exports = quickSort;
