cdf689835586c30fb3e6ca3842598f38a4b11683
adam smith <adam00199382818@gmail.com>
2023-11-03 11:38:26 +0000

    completed 09-05

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,45 @@
-function insertionSort() {}
+function insertionSort(numbers) {
+  // const sortedNumbers = [];
+  //
+  // for (let i = 0; i < numbers.length; i++) {
+  //   let inserted = false;
+  //   const unsortedNumber = numbers[i];
+  //
+  //   if (sortedNumbers.length === 0) {
+  //     sortedNumbers.push(unsortedNumber);
+  //     continue;
+  //   } 
+  //
+  //   for (let j = 0; j < sortedNumbers.length; j++) {
+  //     const sortedNumber = sortedNumbers[j];
+  //
+  //     if (unsortedNumber < sortedNumber) {
+  //       sortedNumbers.splice(j, 0, unsortedNumber);
+  //       inserted = true;
+  //       break;
+  //     }
+  //   }
+  //
+  //   if (!inserted) {
+  //     sortedNumbers.push(unsortedNumber);
+  //   }
+  // }
+  //
+  // return sortedNumbers;
+
+  for (let i = 1; i < numbers.length; i++) {
+    const number = numbers[i];
+    let j = i - 1;
+
+    while (j >= 0 &&  numbers[j] > number) {
+      numbers[j + 1] = numbers[j];
+      j--;
+    }
+
+    numbers[j + 1] = number;
+  }
+
+  return numbers;
+}
 
 module.exports = insertionSort;
